cfbc150e9027b470b352573724accbd7b91015fb
Kiprotich-Lawi <kiprotichlawi7483@gmail.com>
2022-12-16 05:17:19 -0800

    Added file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - Entry point
  * Description: prints '_putchar \n'
@@ -17,4 +17,5 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 
+return (0);
 }
