3b40386f74a55c7f3b03e41951fb8ea7385f25a3
Lawi Kiprotich <fleeklawi@gmail.com>
2023-07-19 20:17:29 +0300

    Added tasks

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the no of arguments passed to it
+ * 
+ * Return: Always 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+    printf("%d\n", argc - 1);
+    return (0);
+}
