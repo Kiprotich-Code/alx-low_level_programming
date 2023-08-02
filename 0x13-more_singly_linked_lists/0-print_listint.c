fef64a66095865911cc0f13d5df018d1cff9dd9e
Kiprotich <fleeklawi@gmail.com>
2023-08-02 04:09:59 -0700

    Initial error fix - task 00

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,27 @@
-#include "lists.h" 
-  
- /** 
-  * print_listint - prints all the elements of a linked list 
-  * @h: linked list of type listint_t to print 
-  * 
-  * Return: number of nodes 
-  */ 
- size_t print_listint(const listint_t *h) 
- { 
-         size_t num = 0; 
-  
-         while (h) 
-         { 
-                 printf("%d\n", h->n); 
-                 num++; 
-                 h = h->next; 
-         } 
-  
-         return (num); 
- }
+#include "lists.h"
+/**
+  *print_list - prints all the elements of a list_t.
+  *@h: pointer to list.
+  *
+  *Return: the number of nodes.
+  */
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			count++;
+		}
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
