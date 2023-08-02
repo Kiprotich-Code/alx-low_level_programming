0d07976ba32baa73e0f3aeede39ba449898102f6
Kiprotich <fleeklawi@gmail.com>
2023-08-02 04:55:35 -0700

    Update - Task 07

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,27 +1,61 @@
 #include "lists.h"
 /**
-  * pop_listint - delets the head node of a listint_t
-  * @head: pointer to pointer of head node.
+  * listint_len - returns the number of elements ina linked listint_t list.
+  * @h: pointer to first element on list.
   *
-  * Return: 0 if list is empty.
-  * headnode data (n).
+  * Return: number of elements in list.
   */
-int pop_listint(listint_t **head)
+size_t listint_len(const listint_t *h)
 {
-	listint_t *temp;
-	int removed = 0;
+	int count;
 
-	if (*head == NULL)
-		return (removed);
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
 
-	if (*head == NULL)
-		return (removed);
+/**
+  * get_nodeint_at_index - returns nth node of a listint_t linked list.
+  * @head: pointer to first elemnt of list.
+  * @index: index of node.
+  *
+  * Return: nth node,
+  * NULL if not found.
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current;
+	unsigned int count;
 
-	temp = *head;
-	removed = temp->n;
-	/*make head point to the next element*/
-	*head = (*head)->next;
-	free(temp);
+	current = head;
+	count = listint_len(head);
 
-	return (removed);
+	if (current == NULL)
+		count++;
+
+	if (count <= index)
+	{
+		return (NULL);
+	}
+	else
+	{
+		count = 0;
+		while (current != NULL)
+		{
+			if (count == index)
+			{
+				break;
+			}
+			else
+			{
+				count++;
+				current = current->next;
+			}
+		}
+		return (current);
+	}
 }
